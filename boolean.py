9afecee223a2d993bd2f2216ecf39c2c58853fea
SingleHe1990 <LargerStudet@gmail.com>
2017-11-15 22:12:27 +0800

    base for python

---
+++ boolean.py
@@ -0,0 +1,9 @@
+age = 27
+if age >= 18:
+	print('adult')
+else:
+	print('teenager')
+print(None)
+he = "SingleHe"
+he = 27
+print("he =",he)
---
+++ learning.py
@@ -0,0 +1,13 @@
+def normalize(name):
+	return name.capitalize()
+
+
+L1 = ['single','JOECHEN','hE']
+L2 = list(map(normalize,L1))
+print(L2)
+
+print('SingleHe','love','JoeChen')
+print('100 + 300 = ',100+300)
+
+name = input('请输入您的名字：')
+print('Hello',name)
